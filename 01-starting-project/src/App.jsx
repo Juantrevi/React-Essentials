1ef6d0b6cb219332687d1c4cecb46f40bb14cd60
Juantrevi <juantrevi70@gmail.com>
2024-05-15 10:17:48 +0100

    Random dynamic word for fundamental

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,17 @@
 
+const readDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
+                {readDescription[getRandomInt(3)]} React concepts you will need for almost any app you are
                 going to build!
             </p>
         </header>
